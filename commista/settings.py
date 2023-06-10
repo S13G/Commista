792fb3b5a1633576ba27359e59c5d073d82fbc51
s13g <ayflix0@gmail.com>
2023-06-10 04:29:06 +0100

    admin and additional docs

--- commista/settings.py
+++ commista/settings.py
@@ -92,15 +92,7 @@ REST_FRAMEWORK = {
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "COMMISTA API",
-    "DESCRIPTION":
-        """
-        Commista is a versatile commerce API designed to facilitate seamless online shopping experiences. With Commista, 
-        administrators have full control over product management, including the ability to add new products, create 
-        attractive offers, and organize items into categories. Additionally, administrators can easily notify customers 
-        about promotions, updates, and other important information. Customers can explore the diverse range of products, 
-        conveniently add items to their cart, and enjoy a hassle-free checkout process with secure payment options. 
-        Commista empowers businesses to deliver a comprehensive and engaging e-commerce platform to their customers.
-        """,
+    "DESCRIPTION": """Commista is a versatile commerce API designed to facilitate seamless online shopping experiences. With Commista, administrators have full control over product management, including the ability to add new products, create attractive offers, and organize items into categories. Additionally, administrators can easily notify customers about promotions, updates, and other important information. Customers can explore the diverse range of products, conveniently add items to their cart, and enjoy a hassle-free checkout process with secure payment options. Commista empowers businesses to deliver a comprehensive and engaging e-commerce platform to their customers.""",
     "VERSION": "1.0.0",
     "CONTACT": "lookouttest91@zohomail.com",
     "SCHEMA_PATH_PREFIX": r'/api/v[0-9]',
