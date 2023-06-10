10ca15af8584bd46be96fa2244c5894d982a5f72
s13g <ayflix0@gmail.com>
2023-06-10 16:10:59 +0100

    render deployment

--- commista/settings.py
+++ commista/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["commista.osc-fr1.scalingo.io", "127.0.0.1", "https://commista.onrender.com"]
+ALLOWED_HOSTS = ["commista.osc-fr1.scalingo.io", "127.0.0.1", "commista.onrender.com"]
 
 # Application definition
 
