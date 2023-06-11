31f05eed8810bb6b2db3d39cead142729551d102
s13g <ayflix0@gmail.com>
2023-06-11 05:04:11 +0100

    Update render deployment

--- core/management/commands/createsu.py
+++ core/management/commands/createsu.py
@@ -1,13 +1,15 @@
 from decouple import config
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+
+User = get_user_model()
 
 
 class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
+        if not User.objects.filter(email=config('ADMIN_EMAIL')).exists():
             User.objects.create_superuser(
                     email=config('ADMIN_EMAIL'),
                     password=config('ADMIN_PASSWORD')
