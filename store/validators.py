0a28b5a5455666df6c46c89f9a43fd85323ef687
s13g <ayflix0@gmail.com>
2023-06-13 08:44:26 +0100

    made some adjustments

--- store/validators.py
+++ store/validators.py
@@ -2,10 +2,8 @@ from django.core.exceptions import ValidationError
 
 
 def validate_image_size(image):
-    max_size = 3 * 1024 * 1024  # 3MB in bytes
+    max_size = 5 * 1024 * 1024  # 3MB in bytes
     if image.size is None:
         raise ValidationError("Please insert an image")
-    elif image.size < 100_000:
-        raise ValidationError(f"Image is too small(less than 100kb)")
     elif image.size > max_size:
-        raise ValidationError(f"Image size should be less than 3MB")
+        raise ValidationError(f"Image size should be less than 5MB")
